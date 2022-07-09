654ac4acec5a170bc1893a2841a426b4f5890dd3
Parsh <parsh.cosmos11@gmail.com>
2022-07-09 18:04:17 +0530

    searching: essentials

--- Problem Solving/LeetCode/SortingAndSearching/33.search-in-rotated-sorted-array.js
+++ Problem Solving/LeetCode/SortingAndSearching/33.search-in-rotated-sorted-array.js
@@ -7,19 +7,20 @@
 // @lc code=start
 /**
  * note: When we divide the rotated array into two halves, using mid index,
- * at least one of subarray(either left or right) would remain sorted ALWAYS.
+ * at least one of subarray(either left or right) would remain sorted ALWAYS(even as we keep dividing the array into subarrays).
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let left = 0;
+var search = function (nums, target) {
+  let left = 0;
   let right = nums.length - 1;
 
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) return mid;
-    
+
+    // search further in the sorted half
     if (nums[left] <= nums[mid]) {
       // left subarray is sorted
       if (nums[left] <= target && target < nums[mid]) right = mid - 1;
@@ -34,4 +35,3 @@ var search = function(nums, target) {
   return -1;
 };
 // @lc code=end
-
