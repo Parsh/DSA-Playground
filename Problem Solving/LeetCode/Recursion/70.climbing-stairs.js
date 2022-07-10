407048c394d8f38f63ca4c99bbbaf7ab4c1bd63b
Parsh <parsh.cosmos11@gmail.com>
2022-07-10 22:34:31 +0530

    simple ques

---
+++ Problem Solving/LeetCode/Recursion/70.climbing-stairs.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var climbStairs = function (n, memo = {}) {
+  if (n <= 1) return 1; // there's only one way to reach staircase 1 or staircase 0
+  if (memo[n]) return memo[n];
+
+  memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+  return memo[n];
+};
+// @lc code=end
---
+++ Problem Solving/LeetCode/SortingAndSearching/278.first-bad-version.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=278 lang=javascript
+ *
+ * [278] First Bad Version
+ */
+
+// @lc code=start
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 1;
+    let right = n;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (!isBadVersion(mid)) left = mid + 1;
+      else right = mid;
+    }
+    return right;
+  };
+};
+// @lc code=end
