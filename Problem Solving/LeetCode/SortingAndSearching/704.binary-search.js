5463f578a0b1c6877890b3757a9c9eed6f000f52
Parsh <parsh.cosmos11@gmail.com>
2022-07-11 12:40:06 +0530

    binary search: optimized

--- Problem Solving/LeetCode/SortingAndSearching/704.binary-search.js
+++ Problem Solving/LeetCode/SortingAndSearching/704.binary-search.js
@@ -6,6 +6,7 @@
 
 // @lc code=start
 /**
+ * note: a comprehensive discussion on Binary Search: https://leetcode.com/problems/binary-search/discuss/423162
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
@@ -14,13 +15,14 @@ var search = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-
-    if (nums[mid] < target) left = mid + 1;
-    else right = mid - 1;
+  // exits once left === right
+  while (left < right) {
+    // const mid = Math.floor((lo + hi) / 2) // worst way to calculate mid: very easy to overflow
+    const mid = left + Math.floor((right - left) / 2);
+    if (target > nums[mid]) left = mid + 1; // exclude mid
+    else right = mid;
+    // console.log({ left, mid, right });
   }
-  return -1;
+  return nums[left] === target ? left : -1;
 };
 // @lc code=end
