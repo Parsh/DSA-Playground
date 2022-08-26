344ce0eee400bdb2bb55fff4ea8e4e36e5d1e2fb
Parsh <parsh.cosmos11@gmail.com>
2022-08-26 16:09:38 +0530

    Word list II

--- Problem Solving/LeetCode/Graph/126.word-ladder-ii.js
+++ Problem Solving/LeetCode/Graph/126.word-ladder-ii.js
@@ -7,83 +7,97 @@
 // @lc code=start
 /**
  * approach: https://leetcode.com/problems/word-ladder-ii/discuss/2424910/Explanation-with-Animation
+ * issue: DFS leads to TLE when wordList is large
  * @param {string} beginWord
  * @param {string} endWord
  * @param {string[]} wordList
  * @return {string[][]}
  */
 var findLadders = function (beginWord, endWord, wordList) {
-  // to check if two words can connect
-  let connected = (a, b) => {
-    let c = 0;
-    for (let i = 0; i < a.length && c < 2; i++) {
-      if (a[i] !== b[i]) c++;
+  if (!wordList.includes(endWord)) return [];
+  wordList.push(beginWord);
+
+  // form the adjacency list
+  const adjacencyList = {};
+  for (let word of wordList) {
+    for (let i = 0; i < word.length; i++) {
+      const pattern = word.slice(0, i) + "*" + word.slice(i + 1);
+      if (adjacencyList[pattern]) adjacencyList[pattern].push(word);
+      else adjacencyList[pattern] = [word];
     }
-    return c == 1;
-  };
+  }
 
-  // dictionary to help us search words faster
-  // and to trackback what word was used
-  let dict = new Set(wordList);
-  if (dict.has(endWord) == false) return [];
+  // find length of the shortest path from begin to end using BFS
+  const shortestPathLength = getShortestPathLength(
+    beginWord,
+    endWord,
+    adjacencyList
+  );
 
-  dict.delete(beginWord);
-  let queue = [beginWord];
-  let paths = [];
+  // find all the paths using DFS, where maximum depth is equal to the shortest path length
+  return getShortestPaths(
+    beginWord,
+    endWord,
+    [],
+    adjacencyList,
+    shortestPathLength
+  );
+};
 
-  // find all ways from beginning
-  // level by level, until reach end at a level
-  let reached = false;
-  while (queue.length && !reached) {
-    // update points of paths for this level
-    paths.push(queue.slice());
+const getShortestPathLength = (beginWord, endWord, adjacencyList) => {
+  const visited = new Set([beginWord]);
+  const queue = [beginWord];
+  let smallestPathLength = 1;
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const word = queue.shift();
+      if (word === endWord) return smallestPathLength; // return count as soon as we find the smallest path to endword
 
-    // access whole level
-    let qlen = queue.length;
-    for (let i = 0; i < qlen && !reached; i++) {
-      // get the words in the record from the previous level
-      let from = queue.shift();
-      for (let to of dict) {
-        // check if those word can connect to any word in dict
-        if (connected(from, to) == false) continue;
-
-        // if yes,
-        // and one of them is endWord then we can stop from here
-        if (to == endWord) {
-          reached = true;
-          break;
+      for (let i = 0; i < word.length; i++) {
+        const pattern = word.slice(0, i) + "*" + word.slice(i + 1);
+        for (let neighbour of adjacencyList[pattern]) {
+          if (!visited.has(neighbour)) {
+            visited.add(neighbour);
+            queue.push(neighbour);
+          }
         }
-
-        // otherwise,
-        // update words for this level
-        queue.push(to);
-
-        // and delete "to" from dict to prevent a cycle
-        dict.delete(to);
       }
+    }
+    smallestPathLength++;
+  }
+  return -1;
+};
+
+const getShortestPaths = (
+  currentWord,
+  endWord,
+  path,
+  adjacencyList,
+  shortestPathLength,
+  ladders = []
+) => {
+  path.push(currentWord);
+  if (path.length === shortestPathLength) {
+    if (currentWord === endWord) ladders.push(path);
+    return;
+  }
+
+  for (let i = 0; i < currentWord.length; i++) {
+    const pattern = currentWord.slice(0, i) + "*" + currentWord.slice(i + 1);
+    for (const word of adjacencyList[pattern]) {
+      if (path.includes(word)) continue;
+      getShortestPaths(
+        word,
+        endWord,
+        [...path],
+        adjacencyList,
+        shortestPathLength,
+        ladders
+      );
     }
   }
 
-  // scanned all but did not see endWord in paths
-  if (!reached) return [];
-
-  // now, every path we choose in the paths
-  // will lead us back to the beginning
-  // so just try to traverse backward from end to begin
-  // via the connected words
-  let ans = [[endWord]];
-  for (let level = paths.length - 1; level >= 0; level--) {
-    let alen = ans.length;
-    for (let a = 0; a < alen; a++) {
-      let p = ans.shift();
-      let last = p[0];
-      for (let word of paths[level]) {
-        if (!connected(last, word)) continue;
-        ans.push([word, ...p]);
-      }
-    }
-  }
-
-  return ans;
+  return ladders;
 };
 // @lc code=end
