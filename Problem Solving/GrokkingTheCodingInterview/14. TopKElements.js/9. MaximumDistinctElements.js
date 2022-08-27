fe20dd410f8692321af34369e46bb63d9ce8f2f8
Parsh <parsh.cosmos11@gmail.com>
2022-08-27 07:18:25 +0530

    max distinct elements

---
+++ Problem Solving/GrokkingTheCodingInterview/14. TopKElements.js/9. MaximumDistinctElements.js
@@ -0,0 +1,42 @@
+/**
+ * Problem Statement:
+ * Given an array of numbers and a number ‘K’, we need to remove ‘K’ numbers
+ * from the array such that we are left with maximum distinct numbers.
+ */
+
+const {
+  MODIFIED_BINARY_HEAP_TYPES,
+  ModifiedBinaryHeap,
+} = require("./utils/ModifiedBinaryHeap");
+
+const maximumDistinctElements = (nums, k) => {
+  if (nums.length <= k) return 0;
+
+  const frequencyCounter = {};
+  for (let num of nums)
+    frequencyCounter[num] = (frequencyCounter[num] || 0) + 1;
+
+  const heap = new ModifiedBinaryHeap(MODIFIED_BINARY_HEAP_TYPES.MIN);
+  let distinct = 0;
+  for (let key in frequencyCounter) {
+    if (frequencyCounter[key] > 1) heap.insert(key, frequencyCounter[key]);
+    else distinct++;
+  }
+
+  // remove duplicate numbers
+  while (k > 0 && heap.values.length) {
+    const { val } = heap.extractTop();
+    if (k < val - 1) break; // insufficient k to make remaining duplciate elements distinct
+    k -= val - 1;
+    distinct++;
+  }
+
+  distinct -= k; // if k still not equal to zero, then we'll have to remove distinct numbers
+
+  return distinct;
+};
+
+// SMOKE TEST
+const nums = [3, 5, 12, 11, 12];
+const k = 3;
+console.log(maximumDistinctElements(nums, k));
